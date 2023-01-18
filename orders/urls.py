12082d6ecef551d9cf3997e4141928cf03086e9f
MaximShaybakov <momon_boton@mail.ru>
2023-01-18 17:46:58 +0500

    add views and serialaizers

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import PartnerUpdate
+from .views import PartnerUpdate, RegisterAccount, LoginAccount, AccountDetails
 
 urlpatterns = [
-    path('partner/update/', PartnerUpdate.as_view()),
+    path('partner/update/', PartnerUpdate.as_view(), name='partner-update'),
+    path('user/register/', RegisterAccount.as_view(), name='user-register'),
+    path('user/login/', LoginAccount.as_view(), name='user-login'),
+    path('user/details/', AccountDetails.as_view(), name='user-details'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from .models import Shop, Category, ProductInfo, ProductParameter, Product, Parameter
+from .serializers import UserSerializer
+# from .signals import new_user_registered
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.password_validation import validate_password
 from rest_framework.views import APIView
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
 from yaml import Loader, load as load_yaml
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
 import requests
 
 # Create your views here.
@@ -14,13 +21,13 @@ class PartnerUpdate(APIView):
     Класс для обновления прайса от поставщика
     """
     def post(self, request, *args, **kwargs):
-        # if not request.user.is_authenticated:
-        #     return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
-
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
+        
         # if request.user.type != 'shop':
         #     return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
         
-        # print(request.data)
+        
         url = request.data.get('url')
         if url:
             validate_url = URLValidator()
@@ -58,3 +65,105 @@ class PartnerUpdate(APIView):
                 return JsonResponse({'Status': True})
 
         return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
+    
+    
+class RegisterAccount(APIView):
+    """
+    Для регистрации покупателей
+    """
+    # Регистрация методом POST
+    def post(self, request, *args, **kwargs):
+
+        # проверяем обязательные аргументы
+        if {'first_name', 'last_name', 'email', 'password', 'company', 'position'}.issubset(request.data):
+            errors = {}
+
+            # проверяем пароль на сложность
+
+            try:
+                validate_password(request.data['password'])
+            except Exception as password_error:
+                error_array = []
+                # noinspection PyTypeChecker
+                for item in password_error:
+                    error_array.append(item)
+                return JsonResponse({'Status': False, 'Errors': {'password': error_array}})
+            else:
+                # проверяем данные для уникальности имени пользователя
+                request.POST._mutable = True
+                request.data.update({})
+                user_serializer = UserSerializer(data=request.data)
+                if user_serializer.is_valid():
+                    # сохраняем пользователя
+                    user = user_serializer.save()
+                    user.set_password(request.data['password'])
+                    user.save()
+                    # new_user_registered.send(sender=self.__class__, user_id=user.id)
+                    return JsonResponse({'Status': True})
+                else:
+                    return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
+
+
+class LoginAccount(APIView):
+    """
+    Класс для авторизации пользователей
+    """
+    # Авторизация методом POST
+    def post(self, request, *args, **kwargs):
+
+        if {'email', 'password'}.issubset(request.data):
+            user = authenticate(request, username=request.data['email'], password=request.data['password'])
+
+            if user is not None:
+                if user.is_active:
+                    token, _ = Token.objects.get_or_create(user=user)
+
+                    return JsonResponse({'Status': True, 'Token': token.key})
+
+            return JsonResponse({'Status': False, 'Errors': 'Не удалось авторизовать'})
+
+        return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
+    
+    
+class AccountDetails(APIView):
+    """
+    Класс для работы данными пользователя
+    """
+
+    # получить данные
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
+
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
+
+    # Редактирование методом POST
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
+        # проверяем обязательные аргументы
+
+        if 'password' in request.data:
+            errors = {}
+            # проверяем пароль на сложность
+            try:
+                validate_password(request.data['password'])
+            except Exception as password_error:
+                error_array = []
+                # noinspection PyTypeChecker
+                for item in password_error:
+                    error_array.append(item)
+                return JsonResponse({'Status': False, 'Errors': {'password': error_array}})
+            else:
+                request.user.set_password(request.data['password'])
+
+        # проверяем остальные данные
+        user_serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return JsonResponse({'Status': True})
+        else:
+            return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
