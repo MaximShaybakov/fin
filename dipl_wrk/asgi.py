ac2855499aabaf9019aecac582439270f2fa7f7d
MaximShaybakov <momon_boton@mail.ru>
2023-01-18 14:40:40 +0500

    init

---
+++ dipl_wrk/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for dipl_wrk project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dipl_wrk.settings')
+
+application = get_asgi_application()
