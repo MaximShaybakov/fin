400365952696a425f66b3edc1446a74a4a2b8789
MaxSh <98151920+MaximShaybakov@users.noreply.github.com>
2023-03-01 19:08:35 +0300

    fix user serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -18,7 +18,7 @@ class UserSerializer(ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email', 'company', 'position', 'contacts', 'password')
+        fields = ('id', 'first_name', 'last_name', 'email', 'company', 'position', 'contacts', 'password', 'type')
         read_only_fields = ('id',)
         
         
