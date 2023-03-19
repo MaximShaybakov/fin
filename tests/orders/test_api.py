2e60fb67e15adf5cff3543344a1b9eeec9628277
MaximShaybakov <momon_boton@mail.ru>
2023-03-20 01:53:49 +0500

    *

--- tests/orders/test_api.py
+++ tests/orders/test_api.py
@@ -1,16 +1,17 @@
 import pytest
+from model_bakery import baker
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from orders.models import User
 
 
-USER_DATA = {"first_name": "fn_user3",
-             "last_name": "ln_user3",
-             "email": "email_user3@mail.ru",
-             "password": "kfJU*4d$#6",
-             "company": "company_user3",
-             "position": "position_user3"}
+USER_DATA = {"first_name": "fn_test_user",
+             "last_name": "ln_test_user",
+             "email": "email_test_user@mail.ru",
+             "password": "password_test_user*",
+             "company": "company_test_user",
+             "position": "position_test_user"}
 
 
 @pytest.fixture
@@ -20,9 +21,17 @@ def _user():
 
 
 @pytest.fixture
-def _token(_user):
+def _headers(_user):
     token = Token.objects.create(user=_user)
-    return token
+    headers = {'HTTP_AUTHORIZATION': f'Token {token}'}
+    return headers
+
+
+@pytest.fixture
+def user_factory():
+    def factory(*args, **kwargs):
+        return baker.make(User, *args, **kwargs)
+    return factory
 
 
 @pytest.fixture
@@ -36,25 +45,52 @@ def client():
 
 
 @pytest.mark.django_db
-def test_register_account(client, url):
+def test_register_account(client, url, user_factory):
     # post method
+    # users = user_factory(_quantity=10)
     count_users = User.objects.count()
     response = client.post(path=f"{url}user/register/",
                            data=USER_DATA)
     data = response.json()
     assert response.status_code == 200
     assert data == {'Status': True}
+    # assert len(data) == len(users)
+    # for index, val in enumerate(data):
+    #     assert val['email'] == users[i].email
     assert User.objects.count() == count_users + 1
 
 
 @pytest.mark.django_db
-def test_login_account(client, url, _token):
+def test_login_account(client, url, _headers):
     # post method
-    client.credentials(HTTP_AUTHORIZATION="Token" + _token)
-    response = client.post(path=f"{url}user/login/")
+    response = client.post(path=f"{url}user/login/", **USER_DATA)
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_account_details(url, client, _headers):
+    # get method
+    response_unauth = client.get(path=f"{url}user/details/")
+    assert response_unauth.status_code == status.HTTP_403_FORBIDDEN
+    response = client.get(path=f"{url}user/details/", **_headers)
     data = response.json()
-    assert response.status_code == status.HTTP_201_CREATED
+    assert response.status_code == status.HTTP_200_OK
+    assert data['email'] == USER_DATA['email']
+    # возврат пароля сериалайзер не обрабатывает
 
 
-# @pytest.mark.django_db
-# def test_account_details():
+@pytest.mark.django_db
+def test_account_details_(url, client, _headers):
+    # post method
+    new_user_data = {"first_name": "fn_user",
+             "last_name": "ln_user",
+             "email": "email_user@mail.ru",
+             "password": "password_user",
+             "company": "company_user",
+             "position": "position_user"}
+    response_unauth = client.post(path=f"{url}user/details/", **_headers, data=new_user_data)
+    assert response_unauth.status_code == status.HTTP_403_FORBIDDEN
+    response = client.get(path=f"{url}user/details/", data=new_user_data, **_headers)
+    data = response.json()
+    assert response.status_code == status.HTTP_200_OK
+    assert data['email'] == USER_DATA['email']
