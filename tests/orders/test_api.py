d0ccab6406d98cb7e33e02f84bc40203c6d9bd20
MaximShaybakov <momon_boton@mail.ru>
2023-03-24 12:16:37 +0500

    *

--- tests/orders/test_api.py
+++ tests/orders/test_api.py
@@ -3,8 +3,8 @@ from model_bakery import baker
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
-from orders.models import User
-import ujson
+from orders.models import User, Category, Product, Shop
+
 
 USER_DATA = {"first_name": "fn_test_user",
              "last_name": "ln_test_user",
@@ -12,6 +12,20 @@ USER_DATA = {"first_name": "fn_test_user",
              "password": "password_test_user*",
              "company": "company_test_user",
              "position": "position_test_user"}
+
+
+@pytest.fixture
+def shop_facrtory():
+    def factory(*args, **kwargs):
+        return baker.make(_model='Shop', *args, **kwargs)
+    return factory
+
+
+@pytest.fixture
+def category_facrtory():
+    def factory(*args, **kwargs):
+        return baker.make(_model='Category', *args, **kwargs)
+    return factory
 
 
 @pytest.fixture
@@ -30,12 +44,12 @@ def _headers(_user):
 @pytest.fixture
 def user_factory():
     def factory(*args, **kwargs):
-        return baker.make(User, *args, **kwargs)
+        return baker.make(_model='User', *args, **kwargs)
     return factory
 
 
 @pytest.fixture
-def url():
+def _url():
     return "http://127.0.0.1:8000/api/v1/"
 
 
@@ -45,53 +59,74 @@ def client():
 
 
 @pytest.mark.django_db
-def test_register_account(client, url, user_factory):
-    # post method
-    # users = user_factory(_quantity=10)
+def test_register_account(client, _url, user_factory):
     count_users = User.objects.count()
-    response = client.post(path=f"{url}user/register/",
+    response = client.post(path=f"{_url}user/register/",
                            data=USER_DATA)
     data = response.json()
     assert response.status_code == 200
     assert data == {'Status': True}
-    # assert len(data) == len(users)
-    # for index, val in enumerate(data):
-    #     assert val['email'] == users[i].email
     assert User.objects.count() == count_users + 1
 
 
 @pytest.mark.django_db
-def test_login_account(client, url, _headers):
-    # post method
-    response = client.post(path=f"{url}user/login/", **USER_DATA)
+def test_login_account(client, _url, _headers):
+    response = client.post(path=f"{_url}user/login/", **USER_DATA)
     assert response.status_code == status.HTTP_200_OK
 
 
 @pytest.mark.django_db
-def test_account_details(url, client, _headers):
-    # get method - get the data
-    response_unauth = client.get(path=f"{url}user/details/")
+def test_account_details(_url, client, _headers):
+    response_unauth = client.get(path=f"{_url}user/details/")
     assert response_unauth.status_code == status.HTTP_403_FORBIDDEN
-    response = client.get(path=f"{url}user/details/", **_headers)
+
+    response = client.get(path=f"{_url}user/details/", **_headers)
     data = response.json()
     assert response.status_code == status.HTTP_200_OK
     assert data['email'] == USER_DATA['email']
-    # возврат пароля сериалайзер не обрабатывает
+    # возврат пароля сериалайзер не обрабатывает поэтому не проверяется
 
 
 @pytest.mark.django_db
-def test_account_details_(url, client, _headers):
-    # post method - partially change the data
+def test_account_details_(_url, client, _headers):
     new_user_data = {"email": "email_user@mail.ru",
                      "password": "password_user",
                      "company": "company_user",
                      "position": "position_user"}
 
-    response_unauth = client.post(path=f"{url}user/details/", data=new_user_data)
+    response_unauth = client.post(path=f"{_url}user/details/", data=new_user_data)
     assert response_unauth.status_code == status.HTTP_403_FORBIDDEN
 
     client.credentials(**_headers)
-    response = client.post(path=f"{url}user/details/", data=new_user_data)
+    response = client.post(path=f"{_url}user/details/", data=new_user_data)
     data = response.json()
     assert response.status_code == status.HTTP_200_OK
     assert data['Data']['email'] == new_user_data['email']
+
+
+@pytest.mark.django_db
+def test_category_view(category_facrtory, _url, client, _headers):
+    category = category_facrtory(_quantity=10)
+    response = client.get(path=f'{_url}categories/')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    client.credentials(**_headers)
+    response = client.get(path=f'{_url}categories/')
+    data = response.json()
+    assert response.status_code == status.HTTP_200_OK
+    assert data['count'] == len(category)
+
+
+@pytest.mark.django_db
+def test_product_info_view(_url, client, _headers):
+    response = client.get(path=f'{_url}products/')
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    client.credentials(**_headers)
+    response = client.get(path=f'{_url}products/')
+    data = response.json()
+    assert response.status_code == status.HTTP_200_OK
+
+
+
+
